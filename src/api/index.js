09a7eb67b3dc8ea090b423f39dd6003745f3e9ee
Deveroonie <sirepiclydev@gmail.com>
2023-11-18 19:10:14 +0000

    add support for arrivals

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const emojis = require('./emojis');
 const departures = require("./departuredelays")
-
+const arrivals = require("./arrivaldelays")
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -13,4 +13,5 @@ router.get('/', (req, res) => {
 
 router.use('/emojis', emojis);
 router.use("/departures/", departures)
+router.use("/arrivals/", arrivals)
 module.exports = router;
